069e9f967f2397c6cf704424fb3c74f74f20de0f
tonocp <amcp88@gmail.com>
2021-12-10 15:05:50 +0100

    Pokemon_Game

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="options-container">
+    <ul>
+      <li v-for="pokemon in pokemons" :key="pokemon.id">{{ pokemon.name.toUpperCase() }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemons: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+li {
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  margin-bottom: 10px;
+  padding: 15px;
+  width: 250px;
+}
+
+li:hover {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+  <div v-else>
+    <h1>¿Quién es este pokémon?</h1>
+
+    <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+
+    <PokemonOptions :pokemons="pokemonArr" />
+  </div>
+</template>
+
+<script>
+import PokemonPicture from "@/components/PokemonPicture";
+import PokemonOptions from "@/components/PokemonOptions";
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+export default {
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+    };
+  },
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions();
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
+};
+</script>
